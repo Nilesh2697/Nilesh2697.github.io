b7aeb4c31cb47924df2acb9b4213116a5780b76e
Nilesh2697 <99721281+Nilesh2697@users.noreply.github.com>
2023-05-08 18:06:26 +0530

    id

--- src/Components/Stats/stats.jsx
+++ src/Components/Stats/stats.jsx
@@ -1,11 +1,10 @@
-
 import React from "react";
 
 import "../Stats/stats.css"
 
 export  const Stats = () => {
   return (
-    <div className="Stats" data-aos="fade-up"
+    <div data-aos="fade-up"
     data-aos-offset="-100"
     data-aos-delay="50"
     data-aos-duration="1000"
@@ -14,36 +13,34 @@ export  const Stats = () => {
     data-aos-once="false"
     data-aos-anchor-placement="top-center" >
          <h1>Github Stats</h1>
-         <div className="parent">
-      <div  className="StatsChild1">
+      <div className="StatsChild1" >
         <a href="https://github.com/Nilesh2697">
-          <img className="image"
+          <img
+          id="github-streak-stats"  
             align="left"
-            id="github-streak-stats"
             src="https://github-readme-streak-stats.herokuapp.com?user=Nilesh2697&theme=radical"
-          />
+            />
         </a>
         <a href="https://github.com/Nilesh2697">
-          <img align="left"
-          id="github-top-langs"
-           className="image"
+          <img 
+            id="github-stats-card"
+            align="left"
             src="https://github-readme-stats.vercel.app/api?username=Nilesh2697&theme=radical&count_private=true&show_icons=true" //stats
           />
         </a>
         
       </div>
  
-      <div className="StatsChild2">
+      <div  className="StatsChild2">
         <a href="https://github.com/Nilesh2697">  
-          <img
+          <img 
+            id="github-top-langs"
             align="center"
-            id="github-stats-card" 
-            src="https://github-readme-stats.vercel.app/api/top-langs/?username=Nilesh2697&theme=radil"  //launguages
+            src="https://github-readme-stats.vercel.app/api/top-langs/?username=Nilesh2697&theme=radical"  //launguages
           />
         </a>
        
       </div>
-    </div>
     </div>
   );
 };
